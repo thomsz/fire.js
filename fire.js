ced31f617f45b46569c8730b6bb2075f8edcb402
Tom Segev <52327695+thomsz@users.noreply.github.com>
2022-03-25 18:55:35 +0100

    feat: wrap template vars for re-activeness (#1)
    * feat: wrap template vars for re-activeness
    
    * feat: only wrap fire variables if they are siblings of other content
    
    * feat: exclude base elements
    
    * feat: refactor
    
    * feat: add initial state getter
    
    * feat: avoid using Array.map()
    
    * feat: add space

--- fire.js
+++ fire.js
@@ -1,56 +1,23 @@
-console.log('Happy coding!')
-
-const app = document.getElementById('app')
-
 class Fire {
   constructor({
     prefix,
+    tagName,
     trackAttribute,
-    initialState
+    state
   } = {}) {
     this._prefix = prefix || 'fire'
+    this._tagName = tagName || 'fire'
     this._trackAttribute = trackAttribute || `${this._prefix}-track-id`
-    this._state = initialState || {}
+    this._initialState = state || {}
+    this._state = state || {}
     this._watchers = {}
     this._elementTracker = {}
 
-    this.setState(initialState)
+    this.setState(state)
   }
 
-  generateTrackId = () => `${this._prefix}_${Math.random().toString(36).substr(2, 9)}`
-
-  setTrackId = (element, trackId) => element.setAttribute(this._trackAttribute, trackId)
-
-  getTrackId = key =>
-    this._elementTracker[key]
-      ? this._elementTracker[key]
-      : this.generateTrackId()
-
-  trackElement = key => this._elementTracker[key] = this.getTrackId(key)
-
-  updateDOM = (key, value) => {
-    const elements = document.getElementsByTagName('*')
-
-    for (let i = 0; i < elements.length; i++) {
-      const elementChildNodes = elements[i].childNodes
-
-      for (let j = 0; j < elementChildNodes.length; j++) {
-        if (elementChildNodes[j].nodeType === 3) {
-          const wrapperElement = elementChildNodes[j].parentElement
-
-          if (elementChildNodes[j].nodeValue.includes(`🔥${key}🔥`)) {
-            elementChildNodes[j].nodeValue = value
-            this.trackElement(key)
-            this.setTrackId(wrapperElement, this._elementTracker[key])
-          } else if (
-            wrapperElement.getAttribute(this._trackAttribute) === this._elementTracker[key] &&
-            elementChildNodes[j].nodeValue !== String(this._state[key])
-          ) {
-            elementChildNodes[j].nodeValue = this._state[key]
-          }
-        }
-      }
-    }
+  get initialState () {
+    return this._initialState
   }
 
   setState = handler => {
@@ -68,6 +35,62 @@ class Fire {
       this.updateDOM(key, value)
       this._watchers[key] && this._watchers[key](this._state[key], oldState[key])
     })
+  }
+
+  generateTrackId = () => `${this._prefix}_${Math.random().toString(36).substr(2, 9)}`
+
+  setTrackId = (element, trackId) => element.setAttribute(this._trackAttribute, trackId)
+
+  getTrackId = key =>
+    this._elementTracker[key]
+      ? this._elementTracker[key]
+      : this.generateTrackId()
+
+  trackElement = key => this._elementTracker[key] = this.getTrackId(key)
+
+  updateDOM = (key, value) => {
+    const excludedElements = ['html', 'head', 'body', 'script']
+    const elements = document.getElementsByTagName('*')
+
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i]
+
+      if (excludedElements.includes(element.nodeName.toLowerCase())) continue
+
+      for (let j = 0; j < element.childNodes.length; j++) {
+        const childNode = element.childNodes[j]
+
+        if (childNode.nodeType === 3) {
+          const wrapperElement = childNode.parentElement
+          const fireVariable = `{{${key}}}`
+
+          if (childNode.nodeValue.includes(fireVariable)) {
+            this.trackElement(key)
+
+            if (childNode.nodeValue.length === fireVariable.length) {
+              wrapperElement.innerHTML = value
+              this.setTrackId(wrapperElement, this._elementTracker[key])
+            } else {
+              wrapperElement.innerHTML = wrapperElement.innerHTML
+                .replace(
+                  fireVariable,
+                  `<${this._tagName}>${value}</${this._tagName}>`
+                )
+
+              this.setTrackId(
+                wrapperElement.getElementsByTagName(this._tagName)[0],
+                this._elementTracker[key]
+              )
+            }
+          } else if (
+            wrapperElement.getAttribute(this._trackAttribute) === this._elementTracker[key] &&
+            childNode.nodeValue !== String(this._state[key])
+          ) {
+            childNode.nodeValue = this._state[key]
+          }
+        }
+      }
+    }
   }
 
   watch = (watchers = {}) => Object.entries(watchers).forEach(([key, watcher]) => (this._watchers[key] = watcher))
